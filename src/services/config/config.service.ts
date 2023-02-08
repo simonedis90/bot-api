eccad48b2247b17d1226dada1e844e083bf9f109
nicholas1990 <nicholas.angelucci@gmail.com>
2023-02-08 23:31:21 +0100

    config

--- src/services/config/config.service.ts
+++ src/services/config/config.service.ts
@@ -7,7 +7,7 @@ export class ConfigService {
   login: string;
   basePath: string;
   keepAlive: string;
-  SCRAPER_PARSER: string;
+  // SCRAPER_PARSER: string;
   SCRAPER_DATABASE: string;
   constructor() {
     const env = process.env;
@@ -16,7 +16,7 @@ export class ConfigService {
     this.production = Boolean(env.production);
     this.basePath = env.api + '/exchange/betting/json-rpc/v1'; // + env.path;
     this.keepAlive = env.keepAlive;
-    this.SCRAPER_PARSER = env.SCRAPER_PARSER;
+    // this.SCRAPER_PARSER = env.SCRAPER_PARSER;
     this.SCRAPER_DATABASE = env.SCRAPER_DATABASE;
   }
 }
